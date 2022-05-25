54686e5b5566a0268310fa73f0a0ff2f74c0b6e0
Dhairya <rupaladhairya@gmail.com>
2022-05-25 17:06:12 +0530

    refactor

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ import images from "./ImageData/imagedata.js";
 const img_list = document.querySelector(".list-container");
 const img_disp = document.querySelector(".display-image");
 const disp_caption = document.querySelector(".displayed-caption");
-const greet = document.getElementsByClassName("greet-toon")[0];
+const greet = document.querySelector(".greet-toon");
 
-//FIXME: use let/const
-var current_img = 0;
-var total_images = 0;
+let current_img = 0;
+let total_images = 0;
 
-// displaying the heading based on current time
+// Toggling the highlight class of given id container
+function toggleHighlight(id){
+  const elmt = document.getElementById(id);
+  elmt.classList.toggle("highlight");
+}
+
+
+// Displaying the WebApp heading based on the current time
 (function makeHeading() {
   let today = new Date();
   let hrs = today.getHours();
@@ -21,12 +27,12 @@ var total_images = 0;
   } else {
     heading = "Good Evening 🌳, Welcome to my Image Viewer";
   }
-  //TODO: get element by id
-  const appHeading = document.getElementsByClassName("heading")[0];
+  const appHeading = document.querySelector(".heading");
   appHeading.innerText = heading;
 })();
 
-// functions for handling the overflow
+
+// Calculating the string caption width in pixels 
 function getTextWidth(text, font) {
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
@@ -35,6 +41,8 @@ function getTextWidth(text, font) {
   return width;
 }
 
+
+// Handling the dynamic overflow in the caption labels
 function handleOverflow(index, caption) {
   let outer_box = document.querySelector(".img-descr-container");
   const box_width = outer_box.clientWidth;
@@ -45,44 +53,45 @@ function handleOverflow(index, caption) {
   const font = window.getComputedStyle(curr_box).font;
   let width = getTextWidth(caption, font);
 
-  let mid = caption.length / 2;
-  while (width > box_width) {
-    caption = caption.slice(0, mid) + "..." + caption.slice(-mid);
-    //TODO: binary search
-    mid--;
-    width = getTextWidth(caption, font);
+  if(width<=box_width) return caption;
+  let r = caption.length-1;
+  let l = 0;
+  let mid = 0;
+  while(l<=r){
+    mid = (l+r)/2;
+    const temp = caption.slice(0,mid) + "..." + caption.slice(-mid);
+    width = getTextWidth(temp,font);
+    if(width>box_width){
+      r = mid-1;
+    }
+    else l=mid+1;
   }
+
+  caption = caption.slice(0,r) + "..." + caption.slice(-r+1);
   return caption;
 }
 
-/**
- * creating side bar content
- * @param {image object} img
- * @returns string content
- */
-function contentMaker(img) {
-  //TODO:  img.previewImage, img.title
+
+// Creating side bar content for the images data
+function contentMaker(index,img) {
   const content = `
         <div class="img-icon-container">
-            <img class="img-icon" src=${img["previewImage"]} alt="Icon can't be loaded">
+            <img class="img-icon" src=${img.previewImage} alt="Icon can't be loaded">
         </div>
         <div class="img-descr-container">
-            <span class="img-descr">${img["title"]}</span>
+            <span class="img-descr" id="descr-${index}">${img.title}</span>
         </div>
     `;
   return content;
 }
 
-/**
- * Displaying the selected current image
- */
+
+// Displaying the current Image in the view bar
 function displayImage() {
-  //TODO:  img.previewImage,
-  //TODO: make image element
   const img_content = `
-        <img class="displayed-image" src=${images[current_img]["previewImage"]} alt=${images[current_img]["title"]}>
+        <img class="displayed-image" src=${images[current_img].previewImage} alt=${images[current_img].title}>
     `;
-  const caption = `${images[current_img]["title"]}`;
+  const caption = `${images[current_img].title}`;
   img_disp.innerHTML = img_content;
   disp_caption.innerHTML = caption;
 }
@@ -90,17 +99,14 @@ function displayImage() {
 // Display first image on loading
 displayImage();
 
-/**
- * loading the sidebar for images list
- */
-// function to get id of list item
 
+// Loading sidebar image list and adding the event listeners
 images.forEach((img, index) => {
+
   const new_div = document.createElement("div");
   new_div.classList.add("list-item");
-  //FIXME: id
-  new_div.setAttribute("id", +index.toString());
-  const content = contentMaker(img);
+  new_div.setAttribute("id", index);
+  const content = contentMaker(index,img);
   new_div.innerHTML = content;
 
   if (index === 0) {
@@ -108,91 +114,79 @@ images.forEach((img, index) => {
   }
   new_div.addEventListener("click", function (event) {
     //TODO: extract function
-    document
-      .getElementById(current_img.toString())
-      .classList.toggle("highlight");
+    toggleHighlight(current_img);
     current_img = +new_div.getAttribute("id");
-    document
-      .getElementById(current_img.toString())
-      .classList.toggle("highlight");
+    toggleHighlight(current_img);
     displayImage();
   });
   img_list.append(new_div);
   total_images += 1;
 });
 
+
+// Truncating the label string of the sidebar
 images.forEach((img, index) => {
-  let curr_descr = document.getElementById(index.toString());
-  curr_descr = curr_descr.lastElementChild;
-  const caption = handleOverflow(index, img["title"]);
+  let curr_descr = document.getElementById(`descr-${index}`);
+  const caption = handleOverflow(index, img.title);
   curr_descr.innerHTML = `
-    <span class="img-descr">${caption}</span>
+    <span class="img-descr" id="descr-${index}">${caption}</span>
     `;
 });
 
+
 // Event Listener for updating the editable caption with side bar content
 disp_caption.addEventListener("input", function (event) {
-  let target = document.getElementById(current_img.toString()).lastElementChild;
+  let target = document.getElementById(`descr-${current_img}`);
   target.innerText = handleOverflow(current_img, this.innerText);
 
-  //FIXME:
-  let edited_content = document.getElementsByClassName("displayed-caption")[0];
-  //FIXME: .title
-  images[current_img]["title"] = edited_content.innerText;
+  const edited_content = document.querySelector(".displayed-caption");
+  images[current_img].title = edited_content.innerText;
 });
 
-/**
- * Eventlistener for the keypresses
- */
+
+
+// Event Listener for the key presses 
 window.addEventListener("keydown", function (event) {
-  //FIXME: remove
-  // console.log(event);
-  //toggling the highlight and changing the image based on the keypress
-  if (event.key === "ArrowUp") {
-    let img_elmt = document.getElementById(current_img.toString());
-    img_elmt.classList.toggle("highlight");
-    //FIXME:
-    current_img =
-      (((current_img - 1) % total_images) + total_images) % total_images;
-    img_elmt = document.getElementById(current_img.toString());
-    img_elmt.classList.toggle("highlight");
-    displayImage();
-  } else if (event.key === "ArrowDown") {
-    let img_elmt = document.getElementById(current_img.toString());
-    img_elmt.classList.toggle("highlight");
-    current_img = (current_img + 1) % total_images;
-    img_elmt = document.getElementById(current_img.toString());
-    img_elmt.classList.toggle("highlight");
-    //FIXME: call once
-    displayImage();
+  if(event.key=="ArrowUp"){
+      toggleHighlight(current_img);
+      current_img = (current_img+total_images-1)%total_images;
+      toggleHighlight(current_img);
   }
+  else if(event.key=="ArrowDown"){
+      toggleHighlight(current_img);
+      current_img = (current_img+1)%total_images;
+      toggleHighlight(current_img);
+  }
+  displayImage();
 });
+
+
+// toggling the greet button class
+function toggleGreetMsg(elmt){
+  elmt.classList.toggle("greet-message");
+  elmt.classList.toggle("greet-message-display");
+}
+
 
 // Greet Button event handler
 greet.addEventListener("mouseover", function (event) {
-  //TODO: refactor
-  let msg = document.querySelector(".greet-message");
-  msg.classList.toggle("greet-message");
-  msg.classList.toggle("greet-message-display");
+  const msg = document.querySelector(".greet-message");
+  toggleGreetMsg(msg);
 });
 
 greet.addEventListener("mouseout", function () {
-  //TODO: refactor
-  let msg = document.querySelector(".greet-message-display");
-  msg.classList.toggle("greet-message");
-  msg.classList.toggle("greet-message-display");
+  const msg = document.querySelector(".greet-message-display");
+  toggleGreetMsg(msg);
 });
+
+
+
+
 //FIXME: add on container
 window.addEventListener("resize", function () {
   images.forEach((img, index) => {
-    //TODO: use id
-    const curr_descr = document.getElementById(
-      index.toString()
-    ).lastElementChild;
-    const caption = handleOverflow(index, img["title"]);
-    //span, change properties
-    curr_descr.innerHTML = `
-        <span class="img-descr">${caption}</span>
-        `;
+    const curr_descr = document.getElementById(`descr-${index}`);
+    const caption = handleOverflow(index, img.title);
+    curr_descr.innerText = caption;
   });
 });
